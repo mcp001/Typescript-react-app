eabc9a75d4d6cfef0ff0dd3581dbbda2c02bd0ce
Matthew Peterson <84740085+mcp001@users.noreply.github.com>
2021-12-09 16:43:25 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from "./components/List"
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Akira Kurosawa",
+      age: 80,
+      url: 'https://24.media.tumblr.com/tumblr_m6s64sUdqG1qgpddwo1_1280.jpg',
+      note: "Japan's greatest film director"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Directors</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
